3bdd655fb43493d85fd104e682a1809751268e3d
nirmitha balasuriya <104336774+nirmithabalasuriya@users.noreply.github.com>
2026-07-03 13:49:04 +0530

    Updated SocialProof.tsx

--- components/pixel-ceylon/SocialProof.tsx
+++ components/pixel-ceylon/SocialProof.tsx
@@ -17,7 +17,7 @@ export default function SocialProof() {
   return (
     <section style={{ background: '#FAFAFA' }} className="py-24 relative overflow-hidden">
       {/* Organic flowing curved lines - unique pattern */}
-      <svg className="absolute inset-0 w-full h-full pointer-events-none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" style={{ opacity: 0.07 }}>
+      <svg className="absolute inset-0 w-full h-full pointer-events-none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" style={{ opacity: 0.10 }}>
         <defs>
           <pattern id="socialproof-waves" patternUnits="userSpaceOnUse" width="300" height="150">
             <path d="M0 75 Q75 40 150 75 T300 75" fill="none" stroke="#b5e409" strokeWidth="1" />
