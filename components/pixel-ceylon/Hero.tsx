caec6d6703e85a55d9c49fd9ea351c66cc5ea2a4
nirmitha balasuriya <104336774+nirmithabalasuriya@users.noreply.github.com>
2026-07-03 14:15:00 +0530

    Updated Hero.tsx

--- components/pixel-ceylon/Hero.tsx
+++ components/pixel-ceylon/Hero.tsx
@@ -78,7 +78,7 @@ export default function Hero() {
             >
               BUILDING
               <br />
-              <span style={{ WebkitTextStroke: '1.5px #2c6ec0', color: 'transparent' }}>
+              <span style={{ WebkitTextStroke: '1.5px #60A5FA', color: 'transparent' }}>
                 DIGITAL
               </span>
               <br />
