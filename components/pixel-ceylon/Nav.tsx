295ac683a2026f621da89becbc2f5e2080951276
nirmitha balasuriya <104336774+nirmithabalasuriya@users.noreply.github.com>
2026-07-03 14:27:45 +0530

    Updated Nav.tsx

--- components/pixel-ceylon/Nav.tsx
+++ components/pixel-ceylon/Nav.tsx
@@ -53,7 +53,7 @@ export default function Nav() {
               <li key={l.label}>
                 <button
                   onClick={() => handleLink(l.href)}
-                  className="text-sm font-semibold text-[#4C4C4C] group-hover:text-[#60A5FA]  duration-200"
+                  className="text-sm font-semibold text-[#4C4C4C] hover:text-[#60A5FA] duration-200"
                 >
                   {l.label}
                 </button>
